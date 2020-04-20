15789bb23926feea940f2b26588cb0db50e80d62
Michka Popoff <michkapopoff@gmail.com>
2020-04-20 10:58:13 +0200

    gitfs: revision bump for libffi (#20166)

--- Formula/gitfs.rb
+++ Formula/gitfs.rb
@@ -5,7 +5,7 @@ class Gitfs < Formula
   homepage "https://www.presslabs.com/gitfs"
   url "https://github.com/presslabs/gitfs/archive/0.5.2.tar.gz"
   sha256 "921e24311e3b8ea3a5448d698a11a747618ee8dd62d5d43a85801de0b111cbf3"
-  revision 2
+  revision OS.mac? ? 2 : 3
   head "https://github.com/presslabs/gitfs.git"
 
   bottle do
@@ -13,7 +13,6 @@ class Gitfs < Formula
     sha256 "01205b29c991e751cf6be6964df1f9398860606e3c284257c60d25fd13ec360f" => :catalina
     sha256 "d9ff9a6216fa90698e730e0a3b3603e1a58726c6f603fb83e7ee5b5d6d9b7ae0" => :mojave
     sha256 "cf5b89f294c252fea0862b1f5b14420a7077d3f02eecc21eda8e02f701ddf4a3" => :high_sierra
-    sha256 "44e43fed128a69f04e340e9b988417f20810cc76b556afa258e66a07dc879b3f" => :x86_64_linux
   end
 
   depends_on "libgit2"
