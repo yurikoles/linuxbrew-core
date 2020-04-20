d7a8a72d5829cc53ef629ca85073f365696a8fc6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-20 06:10:21 +0000

    termrec: update 0.19 bottle.

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -10,6 +10,7 @@ class Termrec < Formula
     sha256 "1d93149ec34c0bf531da76b0137390ed1f05bf2e35e806f1fe875fe6648c4c2b" => :catalina
     sha256 "e3f9f241763a05de367da2ee91727674e18a126a99480a750b901a21bdad0ffb" => :mojave
     sha256 "d6cb43ed14ec0531824bd4eb55ddc625b5711c28b274ce78eb815501e5f3ebf2" => :high_sierra
+    sha256 "8e69b3b9c275945b60d2e67ab8b165ce485de75c372a76f321f7579878b229c6" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
