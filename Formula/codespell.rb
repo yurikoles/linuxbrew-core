0262d14d46ef0e4ccff646acb9f21ffa67c8ab28
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:34:14 +0000

    codespell: update 1.16.0_3 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -12,6 +12,7 @@ class Codespell < Formula
     sha256 "99881ae6b8146c6d2f1920ebe38326ec956fb6f373e0a24e0edb752aba542b39" => :catalina
     sha256 "5a03b3a7c8ee14881fa1ea4b67704d0669f67947ea4dce63e559563105d9d811" => :mojave
     sha256 "adf3d2cb26a64b62a1cd57a380330bf5328aa49a8591668445e212d970c820d0" => :high_sierra
+    sha256 "bda80e3f1d137ad8caf0cba2e9284ce811ef8823c8f254c4354e009b065613fd" => :x86_64_linux
   end
 
   depends_on "python@3.8"
