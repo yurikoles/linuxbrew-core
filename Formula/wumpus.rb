08696c4da12da1bb4dd7839ec4a5c167596553d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 06:12:48 +0000

    wumpus: update 1.7 bottle.

--- Formula/wumpus.rb
+++ Formula/wumpus.rb
@@ -9,6 +9,7 @@ class Wumpus < Formula
     sha256 "49bc794562f3b9a0ad5799b5fcd2d63a5f866b9b6dc7a4b0d80988c388ee3726" => :catalina
     sha256 "e6881d8d217cebdd71e430c4ec8701d369d1ca03bb8724d30977b467d7422d83" => :mojave
     sha256 "006271b20835150dcf3006041f7053adf26a3ec58f9549029d14c844a53570c4" => :high_sierra
+    sha256 "25d9bcb0a8c5978f425a81be7abc3efa1a8058cb34b2d9628e53eae360a98dcb" => :x86_64_linux
   end
 
   def install
