b8cbd3fc1d69401a4fad06bda143be43c6365561
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:34:50 +0000

    diceware: update 0.9.6_3 bottle.

--- Formula/diceware.rb
+++ Formula/diceware.rb
@@ -12,6 +12,7 @@ class Diceware < Formula
     sha256 "510a0f4aca2e72306d28a54c0c5d5a2d5dd98b6a7c5e8b5a104e637b1499866a" => :catalina
     sha256 "f5dfaf498b1d3097b8b5346b215e81f1af6dcd98da65ec123a8b129690df3650" => :mojave
     sha256 "a5838d0d6d69978d1bd9d56699ae4256af9a56bd5636614e783b4e4379c8365f" => :high_sierra
+    sha256 "e27e65a0056b41043543f929af44918d21896d2b35d006eee9e173cf80d2d0c4" => :x86_64_linux
   end
 
   depends_on "python@3.8"
