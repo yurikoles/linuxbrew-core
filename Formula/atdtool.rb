5338106997b8c0570040811b307e408d6089b897
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:33:43 +0000

    atdtool: update 1.3.3_4 bottle.

--- Formula/atdtool.rb
+++ Formula/atdtool.rb
@@ -12,6 +12,7 @@ class Atdtool < Formula
     sha256 "f02a78aa9dbb19e8ca8670a6890f3d51949d014e6890ba5039fd9695bc1f46ce" => :catalina
     sha256 "17b2ed63f8b4f8aa53e367572b53344fa3e6fa8569e5a9fb48f3a0d5257d04cc" => :mojave
     sha256 "0f301757a596e02344c15171a834557e19e4c4145fd440cd9e960b72993e459a" => :high_sierra
+    sha256 "2acb86b3a89d8a9df54e4e0544bfd740d44d87679affc00fae9abc3cc21d56a1" => :x86_64_linux
   end
 
   depends_on "python@3.8"
